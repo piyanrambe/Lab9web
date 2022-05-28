76a098953d17fea9deffd676052ee345759bdfcc
piyanrambe <alfiansyahrambe@mhs.pelitabangsa.ac.id>
2022-05-28 11:45:49 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" type="text/stylesheet" media="screen">
+</head>
+
+<body>
+  <div class="container">
+    <header>
+      <h1>Modularisasi menggunakan require</h1>
+    </header>
+    <nav>
+      <a href="home.php">Home</a>
+      <a href="Tentang.php">Tentang</a>
+      <a href="Kontak.php">Kontak</a>
+    </nav>
